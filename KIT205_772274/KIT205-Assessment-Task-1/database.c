3dada6cb2cb548568de9555a0066b2e2a1dbff52
Kisara <kisarab@utas.edu.au>
2026-05-17 22:09:59 +1000

    Implement db1_add_route method inside database.c

--- KIT205_772274/KIT205-Assessment-Task-1/database.c
+++ KIT205_772274/KIT205-Assessment-Task-1/database.c
@@ -5,3 +5,32 @@
 #include <string.h>
 #include <stdio.h>
 
+
+// Database 1 - BST of Linked Lists
+
+Database1 new_database1() {
+    Database1 db;
+    db.airports = new_bst();
+    return db;
+}
+
+// Add a route from one airport to another
+void db1_add_route(Database1* db, char* from, char* to) {
+    
+    db->airports = insert_bst(db->airports, from);
+    
+    db->airports = insert_bst(db->airports, to);
+
+    
+    BSTNode* node = find_bst(db->airports.root, from);
+    if (node != NULL) {
+        insert_route(&node->routes, to);
+    }
+
+   
+    BSTNode* to_node = find_bst(db->airports.root, to);
+    if (to_node != NULL) {
+        insert_route(&to_node->routes, from);
+    }
+}
+
