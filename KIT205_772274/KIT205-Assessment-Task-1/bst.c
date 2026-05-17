08f52a32f4b524f69bc03ded487811e4a85550b8
Kisara <kisarab@utas.edu.au>
2026-05-17 19:50:04 +1000

    Implement insert_bst() inside bst.c

--- KIT205_772274/KIT205-Assessment-Task-1/bst.c
+++ KIT205_772274/KIT205-Assessment-Task-1/bst.c
@@ -20,3 +20,44 @@ BSTNode* new_bst_node(char* code) {
 	return node;
 }
 
+// Insert a new airport code into the BST
+
+BST insert_bst(BST tree, char* code) {
+	BSTNode* new_node = new_bst_node(code);
+
+	if (tree.root == NULL) {
+		tree.root = new_node;
+		return tree;
+	}
+
+	BSTNode* current = tree.root;
+
+	while (1) {
+		int cmp = strcmp(code, current->code); // Compare the new code with the current node's code
+		if (cmp < 0) { // New code is less than current node's code alphabetically
+			if (current->left == NULL) {
+				current->left = new_node;
+				break;
+			}
+			current = current->left;
+		}
+		else if (cmp > 0) { // New code is greater than current node's code alphabetically
+			if (current->right == NULL) {
+				current->right = new_node;
+				break;
+			}
+			current = current->right;
+		}
+		else { // Duplicate code, do not insert
+			free(new_node);
+			break;
+		}
+	}
+
+	return tree;
+
+}
+
+
+
+
