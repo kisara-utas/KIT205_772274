60b1b8a2b5e8fe90afea9620070dac682c0f5515
Kisara <kisarab@utas.edu.au>
2026-05-17 19:22:38 +1000

    Add function prototypes for bst.h -- new_bst(), new_bst_node(), insert_bst(), find_bst(), print_bst(), free_bst()

--- KIT205_772274/KIT205-Assessment-Task-1/bst.h
+++ KIT205_772274/KIT205-Assessment-Task-1/bst.h
@@ -15,6 +15,18 @@ typedef struct BSTNode {
 
 } BSTNode;
 
+// The BST itself, which contains a pointer to the root node
+typedef struct BST {
+	BSTNode* root; // Pointer to the root node of the BST
+} BST;
+
+// Function prototypes for BST operations
+BST new_bst(); // Create a new empty BST
+BSTNode* new_bst_node(char* code); // Create a new BST node with the given airport code
+BST insert_bst(BST tree, char* code); // Insert a new airport code into the BST
+BSTNode* find_bst(BSTNode* root, char* code); // Find a node in the BST by airport code
+void print_bst(BSTNode* root); // Print the BST in-order (sorted by airport code)
+void free_bst(BSTNode* root); // Free the memory used by the BST
 
 
 #endif
