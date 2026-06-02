4215d918b521304f680b795db9c73ff74cff86b2
Kisara <kisarab@utas.edu.au>
2026-06-02 19:26:08 +1000

    Add tests for add_edge and load_graph methods inside main.c

--- KIT205_772274/KIT205-Assessment-Task-2/main.c
+++ KIT205_772274/KIT205-Assessment-Task-2/main.c
@@ -18,3 +18,34 @@ void test_new_graph() {
 	printf("PASSED\n");
 
 }
+
+
+void test_add_edge() {
+
+	printf("TEST: add_edge inserts edges correctly...");
+	Graph *g = new_graph(3);
+	add_edge(g, 0, 1, 0.5f);
+	add_edge(g, 0, 2, 0.9f);
+
+	int count = 0;
+	EdgeNodePtr curr = g->edges[0].head;
+	while (curr) { count++; curr = curr->next; }
+	assert(count == 2);
+	assert(g->edges[1].head == NULL);
+
+	free_graph(g);
+	printf("PASSED\n");
+
+}
+
+void test_load_graph() {
+	printf("TEST: load_graph reads graph from file correctly... ");
+	Graph *g = load_graph("data/brain_small.txt");
+	assert(g != NULL);
+	assert(g->V == 8);
+	assert(g->edges[0].head != NULL);
+	
+	free_graph(g);
+	printf("PASSED\n");
+}
+
